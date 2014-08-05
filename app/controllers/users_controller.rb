7996fce246d2118bdd602aec56e1f951d3eb25dc
uratol <uratol@gmail.com>
2014-08-05 19:14:21 +0300

    user ragistration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def index
+    @users = User.all
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User registered"
+      redirect_to '/'
+      #redirect_to @user
+    else
+      flash[:error] = "User not registered"
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
