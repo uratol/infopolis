cd5285ae78afc844ea0d541fe1a187b3bc65da1a
uratol <uratol@gmail.com>
2014-08-05 15:32:07 +0300

    css bootstrap

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+  def full_title(page_title=nil)
+    base_title = 'Infopolis'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
