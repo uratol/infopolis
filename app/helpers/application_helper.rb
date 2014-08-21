7f3dc1dd551c65326a393196ff5b13414e748715
uratol <uratol@gmail.com>
2014-08-21 18:18:36 +0300

    tanks report 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,13 @@ module ApplicationHelper
   end
   
   def isnil(v, v_if_nil)
-    defined?(v) ? (v.nil? ? v_if_nil : v) : v_if_nil
+    v.nil? ? v_if_nil : v
   end
+  
+  def array_hash_key_value(a,key_name,key_value,value_name,isnil_value = 0)
+    h = a.detect{|x| x[key_name.to_s].to_s==key_value.to_s}
+    isnil(h.nil? ? nil : h[value_name.to_s] , isnil_value)
+    #isnil(isnil(a.detect{|x| x[key_name]==key_value},{})[value_name], isnil_value)
+  end 
+  
 end
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -2,11 +2,20 @@ module ReportsHelper
   def format_money(m)
     number_to_currency(m, unit: '', delimiter: ' ')
   end
-  def format_liters(m)
-    number_to_currency(m, unit: '', delimiter: ' ')
+  def format_liters(m, precision = 2)
+    number_to_currency(m, unit: '', delimiter: ' ', precision: precision)
   end
   
   def format_percent(m)
     number_to_currency(m, unit: '', delimiter: ' ', precision: 1)
   end
+
+  def format_datetime(d)
+    d.strftime('%d-%m-%Y %H:%M') if d
+  end
+  
+  def format_number(n, precision = 0)
+    number_to_currency(n, unit: '', delimiter: ' ', precision: precision) if n
+  end
+
 end
