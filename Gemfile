3bd98011e98811ab203b0f5badc548102e6a23d6
uratol <uratol@gmail.com>
2014-08-31 02:42:04 +0300

    charts

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'bootstrap-daterangepicker-rails'
 
 gem 'russian'
 
+gem "chartkick"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- lib/rails_extentions.rb
+++ lib/rails_extentions.rb
@@ -3,4 +3,8 @@ class Array
     h = self.detect{|x| x[key_name.to_s].to_s==key_value.to_s}
     h.nil? ? nil : h[value_name.to_s]
   end
+  
+  def hash_sum value_name
+    self.inject(0){|h, d| h+d[value_name] }
+  end
 end
